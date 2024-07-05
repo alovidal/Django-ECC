447105a8d2f9c4b825c6610df01a6af55680028f
alovidal <135652044+alovidal@users.noreply.github.com>
2024-07-05 00:18:10 -0400

    Add files via upload

--- ECC/urls.py
+++ ECC/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('carro', views.carro, name='carro'),
     path('registro', views.registro, name='registro'),
     path('live', views.live, name='live'),
-    
+    path('login', views.login, name='login'),
+    path('crud', views.crud, name='crud'),
+    path('add_user', views.add_user, name='add_user'),
+    path('update_user', views.update_user, name='update_user'),
 ]
--- ECC/views.py
+++ ECC/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
     return render(request, 'pages/index.html')
@@ -10,13 +11,34 @@ def peleadores(request):
     return render(request, 'pages/peleadores.html')
 
 def tienda(request):
-    return render(request, 'pages/tienda.html')
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request, 'pages/tienda.html', context)
 
 def carro(request):
-    return render(request, 'pages/carro.html')
+    if request.user.is_authenticated:
+        order, created = Order.objects.get_or_create()
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items':items, 'order':order}
+    return render(request, 'pages/carro.html', context)
 
 def registro(request):
     return render(request, 'pages/registro.html')
 
 def live(request):
     return render(request, 'pages/live.html')
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+def crud(request):
+    return render(request, 'pages/crud.html')
+
+def add_user(request):
+    return render(request, 'pages/add_user.html')
+
+def update_user(request):
+    return render(request, 'pages/update_user.html')
