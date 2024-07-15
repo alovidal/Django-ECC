30248aaa7325877335ecd2cef42cd1f96456c408
alovidal <135652044+alovidal@users.noreply.github.com>
2024-07-15 06:53:03 -0400

    NUEVO

--- Django/urls.py
+++ Django/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ECC/', include('ECC.urls')),
     path("", include("ECC.urls")),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
